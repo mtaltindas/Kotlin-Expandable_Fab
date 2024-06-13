6051762dbdf6f3855ea351c1e299e7925680974c
~Talha <mtaltindas@gmail.com>
2024-06-13 12:22:27 +0300

    Swipe to Refresh Function Added

--- app/src/main/java/com/altindas/expandable_fab/MainActivity.kt
+++ app/src/main/java/com/altindas/expandable_fab/MainActivity.kt
@@ -5,27 +5,38 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.TextView
 import android.widget.Toast
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this,R.anim.rotate_open_anim) }
     private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this,R.anim.rotate_close_anim) }
-    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this,R.anim.rotate_open_anim) }
-    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this,R.anim.rotate_open_anim) }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this,R.anim.from_bottom_anim) }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this,R.anim.to_bottom_anim) }
 
     lateinit var add_btn:FloatingActionButton
     lateinit var edit_btn:FloatingActionButton
     lateinit var image_btn:FloatingActionButton
 
+    lateinit var refreshLayout:SwipeRefreshLayout
+    lateinit var textView: TextView
+
+
     private var clicked =false
+    private var number=0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         add_btn=findViewById<FloatingActionButton>(R.id.add_btn)
         edit_btn=findViewById<FloatingActionButton>(R.id.edit_btn)
         image_btn=findViewById<FloatingActionButton>(R.id.image_btn)
+
+        textView=findViewById<TextView>(R.id.textView)
+        refreshLayout=findViewById<SwipeRefreshLayout>(R.id.refreshLayout)
+
 
         add_btn.setOnClickListener {
             onAddButtonClicked()
@@ -37,6 +48,12 @@ class MainActivity : AppCompatActivity() {
         }
         image_btn.setOnClickListener {
             Toast.makeText(this,"img",Toast.LENGTH_SHORT).show()
+        }
+
+        refreshLayout.setOnRefreshListener {
+            number++
+            textView.setText(number.toString())
+            refreshLayout.isRefreshing=false
         }
 
     }
